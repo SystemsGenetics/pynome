d84f33a5f2900aff4731a787f9755f697cff7472
Tyler Biggs <biggstd@gmail.com>
2017-08-24 09:55:42 -0700

    added taxonomy_id function

--- pynome/sra.py
+++ pynome/sra.py
@@ -7,5 +7,23 @@ SRA Helpers
 This module will provide tools that will extract the accession numbers from
 the metadata file, ``species.txt``.
 
+This module works by examining ``species.txt`` as a pandas data frame. It has
+the following columns.
+
+    #name
+    species
+    division
+    **taxonomy_id** <-- The one we are interested in.
+    assembly
+    assembly_accession
+    genebuild
+    variation
+    pan_compara
+    peptide_compara
+    genome_alignments
+    other_alignments
+    core_db
+    species_id
 
 """
+
