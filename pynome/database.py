5eb091a59d5995ec8dba77d31e88050ce1c0bfaa
Tyler Biggs <biggstd@gmail.com>
2017-10-02 11:13:21 -0700

    added self designator to init

--- pynome/database.py
+++ pynome/database.py
@@ -151,12 +151,12 @@ class GenomeDatabase(object):
 
     def __init__(self, download_path, database_path):
         """Initialization of the GenomeDatabase class."""
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
-        if not os.path.exists(database_path):
-            os.makedirs(database_path)
         self.database_path = 'sqlite:///' + database_path
         self.download_path = download_path  # The local download location.
+        if not os.path.exists(self.download_path):
+            os.makedirs(self.download_path)
+        if not os.path.exists(self.database_path):
+            os.makedirs(self.database_path)
         # engine is the path that our database is stored.
         logging.debug('Generating database at: {}'.format(self.database_path))
         engine = create_engine(self.database_path)
