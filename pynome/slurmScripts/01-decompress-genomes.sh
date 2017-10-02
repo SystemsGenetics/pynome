321cd23edf951641595e08cec60e961203c6b5b9
Tyler Biggs <biggstd@gmail.com>
2017-10-02 16:46:35 -0700

    updated slurm script

--- pynome/slurmScripts/01-decompress-genomes.sh
+++ pynome/slurmScripts/01-decompress-genomes.sh
@@ -7,7 +7,7 @@
 #SBATCH --job-name=decompress-%A_%a
 #SBATCH --output=logs/01-decompress.%a.log
 #SBATCH --array=0-1
-srun python3 "/data/ficklin/software/pynome/slurmScripts/decompress.py" /scidas/genomes.db $SLURM_ARRAY_TASK_ID
+python "/data/ficklin/software/pynome/slurmScripts/decompress.py" /scidas/genomes.db $SLURM_ARRAY_TASK_ID
 # NOTES ON SLURM SCRIPTS
 # %A will be replaced by the value of SLURM_ARRAY_JOB_ID
 # %a will be replaced by the value of SLURM_ARRAY_TASK_ID
